9ac0f7710b13b21f9c93f37c13d8ebcb586f3fbb
Shogoohashi <76210851+Shogoohashi@users.noreply.github.com>
2023-04-10 15:16:50 +0900

    指摘箇所の修正

--- src/test/java/com/example/controller/LogoutControllerTest.java
+++ src/test/java/com/example/controller/LogoutControllerTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,10 +22,10 @@ class LogoutControllerTest {
 
         @Test
         @DisplayName("正常系: ログイン状態でログアウトができる。")
-        @WithMockUser(roles = {"GENERAL"})
-        void postLogout() throws Exception {
+        void testPostLogout() throws Exception {
             mockMvc.perform(logout("/logout"))
                     .andExpect(status().isFound())
+                    .andExpect(unauthenticated())
                     .andExpect(redirectedUrl("/login?logout")
                     );
         }
